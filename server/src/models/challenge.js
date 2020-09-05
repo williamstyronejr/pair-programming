07fb4187dc1c07301f2ab4243fa9190460fef261
williamstyronejr <itenory@gmail.com>
2020-09-04 23:03:32 -0400

    Add a tag field

--- server/src/models/challenge.js
+++ server/src/models/challenge.js
@@ -4,10 +4,11 @@ const challengeSchema = new mongoose.Schema({
   title: { type: String },
   prompt: { type: String },
   solution: { type: String },
+  tags: { type: String }, // List sparated by commas
   meta: {
     created: { type: Date, default: Date.now },
-    completed: { type: Number, default: 0 }
-  }
+    completed: { type: Number, default: 0 },
+  },
 });
 
 const Challenge = mongoose.model('challenge', challengeSchema);
