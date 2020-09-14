50aaec0fd2082d1395047767013da92fbff16f8a
williamstyronejr <itenory@gmail.com>
2020-09-14 14:27:38 -0400

    Refactored to be a hook

--- client/src/components/dashboard/ChallengeList.js
+++ client/src/components/dashboard/ChallengeList.js
@@ -1,127 +1,152 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroller';
 import { ajaxRequest } from '../../utils/utils';
 import LoadingComponent from '../shared/Loading';
 import './styles/challengeList.css';
 
-class ChallengeList extends Component {
-  constructor(props) {
-    super(props);
+const ChallengeList = (props) => {
+  const [page, setPage] = useState(0);
+  const [endOfList, setEndOfList] = useState(false);
+  const [challenges, setChallenge] = useState([]);
+  const [loadingList, setLoadingList] = useState(false);
+  const [listError, setListError] = useState(null);
+  const [creatingRoom, setCreatingRoom] = useState(false);
+  const [roomError, setRoomError] = useState(null);
 
-    this.getChallenges = this.getChallenges.bind(this);
+  useEffect(() => {
+    if (challenges.length === 0) getChallenges(0);
+  }, []);
 
-    this.state = {
-      page: 0,
-      endOfList: false,
-      challenges: [],
-      loadingList: false,
-      listError: null,
-      creatingRoom: false,
-      roomError: null,
-    };
-  }
+  const getChallenges = () => {
+    if (loadingList) return; // Stops from sending multiple request to update
 
-  componentDidMount() {
-    if (this.state.challenges.length === 0) {
-      // No challenges loaded, grab challenges
-      this.getChallenges(0);
-    }
-  }
-
-  getChallenges() {
-    const { loadingList, page } = this.state;
-    // Stops from sending multiple request to update
-    if (loadingList) return;
-
-    this.setState({ loadingList: true });
+    setLoadingList(true);
 
     ajaxRequest(`/challenge/list?page=${page}`, 'GET')
       .then((res) => {
         // If list is empty, set end of list to be true
-        if (res.data.length === 0) {
-          return this.setState({ endOfList: true, loadingList: false });
+        if (res.data.challenges.length === 0) {
+          setEndOfList(true);
+          return setLoadingList(false);
         }
 
-        this.setState({
-          page: this.state.page + 1,
-          challenges: [...this.state.challenges, ...res.data],
-          loadingList: false,
-        });
+        setPage(page + 1);
+        setChallenge([...challenges, ...res.data.challenges]);
+        setLoadingList(false);
       })
       .catch((err) => {
-        this.setState({ listError: true, loadingList: false });
+        setListError(true);
+        setLoadingList(false);
       });
-  }
+  };
 
-  createPrivateRoom(cId) {
-    this.setState({ creatingRoom: true });
+  const createPrivateRoom = (cId) => {
+    if (creatingRoom) return; // Stop user from creating multiple rooms at once
+    setCreatingRoom(true);
 
     ajaxRequest(`/challenge/${cId}/create`, 'POST')
       .then((res) => {
         if (!res.data.room) {
-          return this.setState({ roomError: true });
+          setRoomError(true);
         }
         // Redirect user to room page
         this.props.history.push(`/c/${cId}/r/${res.data.room}`);
       })
       .catch((err) => {
-        this.setState({ roomError: true, creatingRoom: false });
+        setRoomError(true);
+        setCreatingRoom(false);
       });
+  };
+
+  // Displays loading indicator
+  if (challenges.length === 0) {
+    return <LoadingComponent error={endOfList} />;
   }
 
-  render() {
-    const { endOfList, challenges } = this.state;
+  const listItems = challenges.map((challenge, index) => (
+    <>
+      <li className="challenge-list__item" key={`challenge-${challenge._id}`}>
+        <div className="challenge-list__details">
+          <h3 className="challenge-list__title">{challenge.title}</h3>
+          <ul className="challenge-list__tags">
+            {challenge.tags.split(',').map((tag) => (
+              <li className="challenge-list__tag">{tag.trim()}</li>
+            ))}
+          </ul>
+        </div>
 
-    // Displays loading indicator
-    if (challenges.length === 0) {
-      return <LoadingComponent error={endOfList} />;
-    }
+        <div className="challenge-list__options">
+          <Link className="challenge-list__link" to={`/c/${challenge._id}`}>
+            Pair Up
+          </Link>
 
-    const listItems = challenges.map((challenge, index) => (
-      <>
-        <li className="challenge-list__item" key={`challenge-${challenge._id}`}>
-          <div className="challenge-list__details">
-            <h3 className="challenge-list__title">{challenge.title}</h3>
-            <ul className="challenge-list__tags">
-              {challenge.tags.split(',').map((tag) => (
-                <li className="challenge-list__tag">{tag.trim()}</li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="challenge-list__options">
-            <Link className="challenge-list__link" to={`/c/${challenge._id}`}>
-              Pair Up
-            </Link>
-
-            <button
-              type="button"
-              className="challenge-list__link"
-              onClick={() => this.createPrivateRoom(challenge._id)}
-            >
-              Solo
-            </button>
-          </div>
-        </li>
-      </>
-    ));
-
-    return (
-      <section className="challenge-list">
-        <ul className="challenge-list">
-          <InfiniteScroll
-            pageStart={0}
-            loadMore={this.getChallenges}
-            hasMore={!endOfList}
-            loader={<LoadingComponent />}
+          <button
+            type="button"
+            className="challenge-list__link"
+            onClick={() => this.createPrivateRoom(challenge._id)}
           >
-            {listItems}
-          </InfiniteScroll>
-        </ul>
-      </section>
-    );
-  }
-}
+            Solo
+          </button>
+        </div>
+      </li>
+    </>
+  ));
+
+  return (
+    <section className="challenge-list">
+      <ul className="challenge-list">
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={getChallenges}
+          hasMore={!endOfList}
+          loader={<LoadingComponent />}
+        >
+          {listItems}
+        </InfiniteScroll>
+      </ul>
+    </section>
+  );
+};
+
+// class ChallengeList extends Component {
+//   render() {
+//     const { endOfList, challenges } = this.state;
+
+//     // Displays loading indicator
+//     if (challenges.length === 0) {
+//       return <LoadingComponent error={endOfList} />;
+//     }
+
+//     const listItems = challenges.map((challenge, index) => (
+//       <>
+//         <li className="challenge-list__item" key={`challenge-${challenge._id}`}>
+//           <div className="challenge-list__details">
+//             <h3 className="challenge-list__title">{challenge.title}</h3>
+//             <ul className="challenge-list__tags">
+//               {challenge.tags.split(',').map((tag) => (
+//                 <li className="challenge-list__tag">{tag.trim()}</li>
+//               ))}
+//             </ul>
+//           </div>
+
+//           <div className="challenge-list__options">
+//             <Link className="challenge-list__link" to={`/c/${challenge._id}`}>
+//               Pair Up
+//             </Link>
+
+//             <button
+//               type="button"
+//               className="challenge-list__link"
+//               onClick={() => this.createPrivateRoom(challenge._id)}
+//             >
+//               Solo
+//             </button>
+//           </div>
+//         </li>
+//       </>
+//     ));
+//   }
+// }
 
 export default withRouter(ChallengeList);
