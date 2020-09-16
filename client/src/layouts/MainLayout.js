17e95465b1b5f77d73f13bb5815211de8b66423e
williamstyronejr <itenory@gmail.com>
2020-09-16 17:08:21 -0400

    Updated imports

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from './Header';
 
 class MainLayout extends React.Component {
   shouldComponentUpdate(nextProps) {
