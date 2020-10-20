747bf5924d39628181926d82a68a52e7a007ba54
williamstyronejr <itenory@gmail.com>
2020-10-20 12:02:35 -0400

    Updated to allow multiple chat locations

--- client/src/components/challenge/ChatRoom.js
+++ client/src/components/challenge/ChatRoom.js
@@ -1,63 +1,121 @@
-import React, { Component } from 'react';
-import ChatIcon from '../shared/ChatIcon';
+import React, { useState } from 'react';
 import './styles/chatRoom.css';
 
-class ChatRoom extends Component {
-  onChange = (e) => {
-    this.props.setMessage(e.target.value);
+const ChatRoom = ({
+  setMessage,
+  sendMessage,
+  visible,
+  toggleChat,
+  chatInput,
+  messages,
+}) => {
+  const [location, setLocation] = useState('aside');
+  const [settingsVisible, setSettingsVisible] = useState(false);
+
+  if (visible) {
+    return (
+      <div className="chat chat--hidden">
+        <svg
+          className="chat__icon"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={toggleChat}
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"
+          />
+        </svg>
+      </div>
+    );
+  }
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
   };
 
   // When user hits enter (without shift), send current message
-  onKeyDown = (e) => {
+  const onKeyDown = (e) => {
     if (e.keyCode === 13 && !e.shiftKey) {
       e.preventDefault(); // Stop '\n' from being put in form
 
       // Prevent sending empty message (spaces work)
-      if (this.props.chatInput) {
-        this.props.sendMessage(this.props.chatInput);
-      }
+      if (chatInput) sendMessage(chatInput);
     }
   };
 
-  render() {
-    // if not visible, render chat icon
-    if (!this.props.visible) {
-      return <ChatIcon toggleChat={this.props.toggleChat} />;
-    }
-
-    const messageList = this.props.messages.map((value, key) => {
-      return (
-        <li className={`message message-${value.type}`} key={key}>
-          <div className={`message-${value.type}__content`}>
-            <p className="content__text">{value.content}</p>
-          </div>
-        </li>
-      );
-    });
-
+  const messageList = messages.map((msg, key) => {
     return (
-      <div className="chat chat-active">
-        <header className="chat-options">
-          <button type="button" onClick={this.props.toggleChat}>
-            close chat
-          </button>
-        </header>
-        <ul className="chat__messages">{messageList}</ul>
-
-        <div className="chat__input">
-          <textarea
-            className="input__text"
-            value={this.props.chatInput}
-            onChange={this.onChange}
-            onKeyDown={this.onKeyDown}
-            placeholder="Type a message ..."
-          />
-
-          <button className="btn input__submit">></button>
+      <li className={`message message-${msg.type}`} key={key}>
+        <div className={`message-${msg.type}__content`}>
+          <p className="content__text">{msg.content}</p>
         </div>
-      </div>
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <div className={`chat chat--${location}`}>
+      <header className="chat__header">
+        <h4 className="chat__heading">
+          <button className="btn btn--chat" onClick={toggleChat}>
+            Chat
+          </button>
+        </h4>
+
+        <div className="chat__options">
+          <button
+            type="button"
+            className="btn btn--options"
+            onClick={() => setSettingsVisible(!settingsVisible)}
+          >
+            ...
+          </button>
+
+          <div
+            className={`chat__settings ${
+              settingsVisible ? 'chat__settings--active' : ''
+            }`}
+          >
+            <div className="chat__layouts">
+              <button
+                className="btn"
+                type="button"
+                onClick={() => {
+                  setLocation('aside');
+                }}
+              >
+                aside
+              </button>
+              <button
+                className="btn"
+                type="button"
+                onClick={() => {
+                  setLocation('detach');
+                }}
+              >
+                detach
+              </button>
+            </div>
+            <hr className="divider" />
+          </div>
+        </div>
+      </header>
+
+      <ul className="chat__messages">{messageList}</ul>
+
+      <div className="chat__input">
+        <textarea
+          className="input__text"
+          value={chatInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          placeholder="Type a message ..."
+        />
+      </div>
+    </div>
+  );
+};
 
 export default ChatRoom;
