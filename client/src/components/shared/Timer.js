3ea6621a404c2d70fb9e31ec322c118a9cc1bd11
williamstyronejr <itenory@gmail.com>
2020-10-20 12:10:38 -0400

    Adds a leading 0 infront of numbers

--- client/src/components/shared/Timer.js
+++ client/src/components/shared/Timer.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import './styles/timer.css';
 
+/**
+ * Converts a number into a string and adds a zero in front.
+ * @param {Number} number A number to convert to string
+ * @return {String} Returns a string of the number with a zero in front.
+ */
 function addLeadingZero(number) {
   return (new Array(3).join('0') + number).slice(-2);
 }
 
 // Not completely accraute
 export const Timer = ({ isPaused }) => {
-  console.log(isPaused);
   const [timer, setTimer] = useState(0);
   let timerInterval;
 
@@ -22,9 +27,11 @@ export const Timer = ({ isPaused }) => {
   }, []);
 
   return (
-    <div className="">
-      <span className="">{addLeadingZero(Math.floor(timer / 60))}</span>:
-      <span className="">{addLeadingZero(timer % 60)}</span>
+    <div className="timer">
+      <span className="timer__hour">
+        {addLeadingZero(Math.floor(timer / 60))}
+      </span>
+      :<span className="timer__minute">{addLeadingZero(timer % 60)}</span>
     </div>
   );
 };
