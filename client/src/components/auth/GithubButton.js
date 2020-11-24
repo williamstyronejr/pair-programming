dc8c0a040ccf4b4c59d1296cab8497b3a02105c9
williamstyronejr <itenory@gmail.com>
2020-11-24 00:24:30 -0500

    Updated to work for both signin/signup

--- client/src/components/auth/GithubButton.js
+++ client/src/components/auth/GithubButton.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const GithubButton = () => (
-  <div>
-    <a href="http://localhost:5000/auth/github/callback">Github login</a>
+const GithubButton = ({ signIn }) => (
+  <div className="btn btn--social">
+    <a
+      className="github-link"
+      href="http://localhost:5000/auth/github/callback"
+    >
+      <span className="flex-wrapper flex-wrapper--center">
+        <i className="github-logo devicon-github-plain-wordmark" />
+        {signIn ? 'Sign in with Github' : 'Sign up with Github'}
+      </span>
+    </a>
   </div>
 );
 
