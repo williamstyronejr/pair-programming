60a14b973fcb893370eb4507c2b37e59513885b4
williamstyronejr <itenory@gmail.com>
2020-11-23 23:53:29 -0500

    Created a language field

--- server/src/models/room.js
+++ server/src/models/room.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
   challenge: { type: String },
+  language: { type: String },
   code: { type: String },
   private: { type: Boolean, default: false },
   users: { type: Array },
   size: { type: Number, default: 2 },
   usersInRoom: { type: Number, default: 0 },
   completed: { type: Boolean, default: false },
-  inviteKey: { type: String }
+  inviteKey: { type: String },
 });
 
 const Room = mongoose.model('room', roomSchema);
