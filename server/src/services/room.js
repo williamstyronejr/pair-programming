78f2271625c878956ca6773278caa5b4d0d63137
williamstyronejr <itenory@gmail.com>
2020-11-23 23:54:07 -0500

    Created a field for initial code based on language

--- server/src/services/room.js
+++ server/src/services/room.js
@@ -2,19 +2,27 @@ const Room = require('../models/room');
 
 /**
  * Creates a room for challenge and adds users into it.
- * @param {string} challengeId Id for challenge room is set to
- * @param {array} users Array of user id that can join the room
- * @param {boolean} isPrivate Flag to indicate if room is private
- * @param {number} size Max number of user in the room
+ * @param {String} challengeId Id for challenge room is set to
+ * @param {Array} users Array of user id that can join the room
+ * @param {String} language Programming language being used
+ * @param {Boolean} isPrivate Flag to indicate if room is private
+ * @param {Number} size Max number of user in the room
  * @return Returns a promise that resolves with the room object.
  */
-exports.createRoom = (challengeId, users, isPrivate = false, size = 2) => {
+exports.createRoom = (
+  challengeId,
+  users,
+  language,
+  isPrivate = false,
+  size = 2
+) => {
   return Room({
     challenge: challengeId,
+    language,
     users,
     private: isPrivate,
     size,
-    userInRoom: users.length
+    userInRoom: users.length,
   }).save();
 };
 
@@ -24,7 +32,7 @@ exports.createRoom = (challengeId, users, isPrivate = false, size = 2) => {
  * @return {Promise<object>} A promise to resolve with a room object, or null
  *  if no room is found.
  */
-exports.findRoom = id => {
+exports.findRoom = (id) => {
   return Room.findById(id).exec();
 };
 
@@ -57,7 +65,7 @@ exports.addUserToRoom = (roomId, userId) => {
     {
       _id: roomId,
       $expr: { $lt: ['$usersInRoom', '$size'] },
-      users: { $ne: userId }
+      users: { $ne: userId },
     },
     { $addToSet: { users: userId }, $inc: { usersInRoom: 1 } }
   ).exec();
@@ -69,8 +77,8 @@ exports.addUserToRoom = (roomId, userId) => {
  * @return {Promise<object>} A promise to resolve with a room object, or null
  *  if no room is found.
  */
-exports.markRoomCompleted = id => {
+exports.markRoomCompleted = (id) => {
   return Room.findByIdAndUpdate(id, {
-    completed: true
+    completed: true,
   }).exec();
 };
