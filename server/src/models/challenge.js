49403436e54dbd3f543fb3ba4e53822f8c991323
williamstyronejr <itenory@gmail.com>
2020-11-24 01:36:06 -0500

    Created a initial code field

--- server/src/models/challenge.js
+++ server/src/models/challenge.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const challengeSchema = new mongoose.Schema({
   title: { type: String },
   prompt: { type: String },
-  solution: { type: String },
   tags: { type: String }, // List sparated by commas
+  initialCode: [
+    {
+      language: { type: String },
+      code: { type: String },
+    },
+  ],
+  isPublic: { type: Boolean, default: false },
   meta: {
     created: { type: Date, default: Date.now },
     completed: { type: Number, default: 0 },
